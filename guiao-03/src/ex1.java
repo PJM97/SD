bff615695d6822abe353e6253ae8c6b6e9ea6e86
Unknown <paulojorgecome@gmail.com>
2019-10-15 09:51:09 +0100

    guiao-03 0/1

---
+++ guiao-03/src/ex1.java
@@ -0,0 +1,132 @@
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Conta{
+    private int id;
+    private double valor;
+    private ReentrantLock lock_Conta;
+
+    Conta(int id){
+        this.id = id;
+        this.valor=0;
+        this.lock_Conta = new ReentrantLock();
+    }
+
+    public synchronized double consultar(){
+        return this.valor;
+    }
+
+    public void depositar(double valor){
+        this.lock_Conta.lock();
+        this.valor+=valor;
+        this.lock_Conta.unlock();
+    }
+
+    public void levantar(double valor){
+        this.lock_Conta.lock();
+        this.valor-=valor;
+        this.lock_Conta.unlock();
+    }
+
+    public void lock()   {this.lock_Conta.lock();}
+    public void unlock() {this.lock_Conta.unlock();}
+}
+
+class Banco{
+    private HashMap<Integer,Conta> contas;
+    private ReentrantLock lock_Banco;
+
+    Banco(int n){
+        this.contas = new HashMap<>();
+        for(int i=0;i<n;i++)
+            this.contas[i] = new Conta();
+        this.lock_Banco = new ReentrantLock();
+    }
+
+    public double consultar(int conta){
+        return this.contas[conta].consultar();
+    }
+
+    public void depositar(int id, double valor) throws ContaInvalida{
+        if(!contas.containsKey(id)) {
+            throw new ContaInvalida(id);
+        }
+
+        this.contas.get(id).lock();
+
+        this.contas.get(id)
+    }
+
+    public void levantar(int conta, double valor){
+        this.contas[conta].levantar(valor);
+    }
+
+    public void transferir(int origem, int destino, double valor){
+        int min = Math.min(origem,destino);
+        int max = Math.max(origem,destino);
+
+        this.contas[min].lock();
+        this.contas[max].lock();
+
+        this.levantar(origem,valor);
+        this.depositar(destino,valor);
+
+        this.contas[max].unlock();
+        this.contas[min].unlock();
+
+    }
+}
+
+
+class Cliente_1 implements Runnable{
+    private Banco banco;
+
+
+    Cliente_1(Banco b){
+        this.banco=b;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +" C0:"+ banco.consultar(0)+" C1:"+ banco.consultar(1));
+        this.banco.transferir(0,1,1000);
+        System.out.println(Thread.currentThread().getName() +" C0:"+ banco.consultar(0)+" C1:"+ banco.consultar(1));
+
+    }
+}
+
+class Cliente_2 implements Runnable{
+    private Banco banco;
+
+    Cliente_2(Banco b){
+        this.banco=b;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +" C0:"+ banco.consultar(0)+" C1:"+ banco.consultar(1));
+        this.banco.transferir(1,0,1000);
+        System.out.println(Thread.currentThread().getName() +" C0:"+ banco.consultar(0)+" C1:"+ banco.consultar(1));
+    }
+}
+
+// Main
+public class ex1 {
+    public static void main(String[] args) throws Exception {
+        int n_contas = 10;
+        Banco banco = new Banco(n_contas);
+        Thread T[] = new Thread[2];
+        T[0] = new Thread(new Cliente_1(banco));
+        T[0].setName("Thread0");
+        T[1] = new Thread(new Cliente_2(banco));
+        T[1].setName("Thread1");
+
+        T[0].start();
+        T[1].start();
+
+        T[0].join();
+        T[1].join();
+
+        System.out.println(banco.consultar(0));
+        System.out.println(banco.consultar(1));
+    }
+}
+
